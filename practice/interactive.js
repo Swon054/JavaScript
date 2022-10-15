b8452eb1ef02ddb457211092f1387d3ee2726468
Sangwon Seo <113398387+Swon054@users.noreply.github.com>
2022-10-15 23:52:09 +0900

    Practice interactive box

---
+++ practice/interactive.js
@@ -0,0 +1,7 @@
+alert("hello"); // hello
+
+let yourAge = prompt("how old are you?", 20);
+alert("Your age is ${yourAge}."); // Your age is 20
+
+let sayYes = confirm("Do you like physics?")
+alert( sayYes); // true
