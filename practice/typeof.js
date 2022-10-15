23ddc9c5e30f620d0ae24d2e303295ea721588eb
Sangwon Seo <113398387+Swon054@users.noreply.github.com>
2022-10-15 23:38:22 +0900

    Check valuable type and 'typeof()'

---
+++ practice/typeof.js
@@ -0,0 +1,9 @@
+let string = "Hello"; //hello
+let number = 10; // 10
+let bigInt = 123213895681385817935351551235n;
+let bool = true;
+
+alert(typeof(string)); // string
+alert(typeof(number)); // number
+alert(typeof(bigInt)); // bigint
+alert(typeof(bool)); // boolean
